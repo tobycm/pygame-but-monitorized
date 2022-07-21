0b791d49635e0d08e13f092b5f85391dd7fd1e08
Toby <newtobycm@gmail.com>
2022-07-20 20:46:09 -0700

    well good times. added new function for angle degree to pygame pos shorten and clean variable names remove some quick start stuff

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from math import cos, sin
+
 from pygame import init as pg_init, display as pg_display, Surface, Color, Rect, event as pg_event, QUIT, draw as pg_draw
 from pygame.time import Clock
 import pygame_gui
@@ -12,8 +14,8 @@ pg_display.set_caption("Yo!")
 window_surface = pg_display.set_mode(res)
 
 # Set background
-background = Surface(res)
-background.fill(Color('#000000'))
+bg = Surface(res)
+bg.fill(Color(0, 0, 0))
 
 # Some important stuff I don't know
 manager = pygame_gui.UIManager(res)
@@ -26,52 +28,55 @@ TR_CRN = (res[0], 0)
 BL_CRN = (0, res[1])
 BR_CRN = (res[0], res[1])
 MID = tuple(pos // 2 for pos in res)
-rectangle = Rect(
+default_rect = Rect(
     (0, 0),
     (100, 50)
 )
 
-def center_rect(rect : Rect = rectangle):
+def center_rect(rect : Rect = default_rect):
     rect.x = MID[0] - rect.width // 2
     rect.y = MID[1] - rect.height // 2
     return rect
 
-# Create a button with text
-hello_button = pygame_gui.elements.UIButton(
-    relative_rect=center_rect(rectangle),
-    text='Say Hello',
-    manager=manager
-)
+def degrees_to_pygame(radius : int, angle : int):
+    x = round(sin(angle * 3.14 / 180) * radius)
+    y = round(cos(angle * 3.14 / 180) * radius)
+    return (x + radius, -y + radius)
 
 # Main loop
 while is_running:
-    time_delta = clock.tick(60)/1000.0
+    clock.tick(60)
+    
+    # Process events
     for event in pg_event.get():
         if event.type == QUIT:
             is_running = False
 
-        if event.type == pygame_gui.UI_BUTTON_PRESSED:
-            if event.ui_element == hello_button:
-                print('Hello World!')
-
         manager.process_events(event)
 
-    manager.update(time_delta)
-
     # Draw the background
-    window_surface.blit(background, (0, 0))
+    window_surface.blit(bg, (0, 0))
     manager.draw_ui(window_surface)
     
     # Draw the circle
     clock_size = 50
     pg_draw.circle(
         window_surface,
-        Color('#ffffff'),
+        Color(255, 255, 255),
         (
             TL_CRN[0] + clock_size,
             TL_CRN[1] + clock_size
         ),
         clock_size
+    )
+    
+    # Draw seconds
+    pg_draw.line(
+        bg,
+        Color(0, 0, 0),
+        (clock_size, clock_size),
+        degrees_to_pygame(clock_size, 45),
+        10
     )
 
     pg_display.update()
